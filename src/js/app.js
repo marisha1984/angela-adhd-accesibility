ac4857498ce7a58e40446b064e50a78058d03f64
Muska <sunmaris@mail.ru>
2023-03-24 14:01:55 +0000

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
-// console.log("App.js here")
+const content = document.querySelectorAll(".adhd-text");
+
+const toADHD = (phrase) => {
+  return phrase
+    .split(' ')
+    .map(word => word.substring(0, 3).bold() + word.slice(3))
+    .join(' ');
+};
+
+content.forEach(element => {
+  element.innerHTML = toADHD(element.textContent)
+});
+
+
+
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./src/*.{html,js}"],
+  content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': 'Source Sans Pro, sans-serif'
+      },
+      colors: {
+        'olive': {
+          100: '#EBFFE2',
+          200: '#C5DFB9',
+          400: '#8CBC76',
+          500: '#70985E'
+        }
+      },
+      lineClamp: {
+        7: '7',
+        8: '8',
+        9: '9',
+        10: '10',
+      }
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
 };
